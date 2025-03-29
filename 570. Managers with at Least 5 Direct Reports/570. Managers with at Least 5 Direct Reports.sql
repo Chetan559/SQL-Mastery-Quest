24b85f11d077b1047fe164d32dbc8c231c0f8e57
Chetan Sharma <cschetan559@gmail.com>
2025-03-29 19:37:27 +0530

    Time: 284 ms (99.27%), Space: 0B (100.00%) - Leetcode

---
+++ 570. Managers with at Least 5 Direct Reports/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+WITH cte as(SELECT managerId, COUNT(managerId) AS direct_reports
+            FROM Employee
+            GROUP BY managerId
+)
+
+SELECT e.name FROM Employee e
+LEFT JOIN cte ON e.id = cte.managerId
+WHERE direct_reports >=5;
